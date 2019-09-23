0ad5e156cca44e542c9c08b76240ee0fd826b04f
EmilioGitHub <davletovemil@yandex.ru>
2019-09-23 19:51:19 +0700

    first commit
    make register correct

--- FoodOrder/FoodOrder/Controllers/AccountController.cs
+++ FoodOrder/FoodOrder/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace FoodOrder.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {

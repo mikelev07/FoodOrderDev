5de7c34d8f817c4d314ddcbe02d90427b01b8fe8
EmilioGitHub <davletovemil@yandex.ru>
2019-09-25 14:48:01 +0700

    UvmAndCvmMigration

---
+++ FoodOrder/FoodOrder/Migrations/201909250747024_UserViewModelAndCompanyViewModel.cs
@@ -0,0 +1,29 @@
+namespace FoodOrder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserViewModelAndCompanyViewModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UserViewModels",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        UserName = c.String(),
+                        SecondName = c.String(),
+                        Email = c.String(),
+                        PhoneNumber = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.UserViewModels");
+        }
+    }
+}

0c01a02633b81c11d833065080350f9bcd687e30
mikelev07 <45894165+mikelev07@users.noreply.github.com>
2019-09-26 14:31:57 +0300

    And New

--- FoodOrder/FoodOrder/Controllers/AccountController.cs
+++ FoodOrder/FoodOrder/Controllers/AccountController.cs
@@ -79,7 +79,10 @@ namespace FoodOrder.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    if (returnUrl == null)
+                        return RedirectToAction("MyDetails", "Users");
+                    else 
+                        return RedirectToLocal(returnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -155,7 +158,7 @@ namespace FoodOrder.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await UserManager.AddToRoleAsync(user.Id, "company");
+                    await UserManager.AddToRoleAsync(user.Id, "cook");
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // Дополнительные сведения о включении подтверждения учетной записи и сброса пароля см. на странице https://go.microsoft.com/fwlink/?LinkID=320771.

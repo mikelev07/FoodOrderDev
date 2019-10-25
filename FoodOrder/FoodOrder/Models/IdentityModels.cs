fed0f6b90bbd6a66dc083830219bea3e96027786
EmilioGitHub <davletovemil@yandex.ru>
2019-10-25 15:01:54 +0700

    MyDetailsEmployee

--- FoodOrder/FoodOrder/Models/IdentityModels.cs
+++ FoodOrder/FoodOrder/Models/IdentityModels.cs
@@ -61,6 +61,40 @@ namespace FoodOrder.Models
         public bool IsCheckInstruction { get; set; }
     }
 
+    public class NewEmployeeViewModel
+    {
+        public string Id { get; set; }
+        /// <summary>Логин</summary>
+        public string UserName { get; set; }
+        /// <summary>Имя</summary>
+        public string Name { get; set; }
+
+        /// <summary>Фамилия</summary>
+        public string Surname { get; set; }
+
+        /// <summary>Отчество</summary>
+        public string Patronymic { get; set; }
+
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Текущий пароль")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать символов не менее: {2}.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Новый пароль")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение нового пароля")]
+        [Compare("NewPassword", ErrorMessage = "Новый пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+
     public class EditUserViewModel
     {
         public string Id { get; set; }

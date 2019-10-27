bf14a5461b6a65bf968fd87c0fe671680f612c42
EmilioGitHub <davletovemil@yandex.ru>
2019-10-27 13:45:24 +0700

    DetailsRepresentative

--- FoodOrder/FoodOrder/Models/IdentityModels.cs
+++ FoodOrder/FoodOrder/Models/IdentityModels.cs
@@ -200,5 +200,7 @@ namespace FoodOrder.Models
         public DbSet<Dish> Dishes { get; set; }
         public DbSet<Menu> Menus { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+        public System.Data.Entity.DbSet<FoodOrder.Models.UserViewModel> UserViewModels { get; set; }
     }
 }
